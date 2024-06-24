12524666d8f59c9cdedf12f5b28aa455ddaf2e37
Yessenia Diaz <167342274+yesDiaz1@users.noreply.github.com>
2024-06-24 18:49:38 -0400

    Update to make it actually work
    Added a way to make sure there are no double hits
    
    Now we have checks at different times.

--- vigilanceTask.py
+++ vigilanceTask.py
@@ -1,19 +1,51 @@
-# Test change
 # Import necessary libraries
 from psychopy import visual, core, event
 import random
-
+import math
 random.seed(0)
 
+#-------------[Creating Functions]-------------#
 # Function to generate random double digit
-def generate_double_digit():
+def generate_digit():
     return random.randint(0, 9)
 
-# Function to determine if the difference between two digits is 0 or 1
+# Function that makes a random percentage for the first check
+def generate_check1_percent():
+    return round(random.uniform(0.50, 0.67), 2)
+
+# Function that makes a random percentage for the second check
+def generate_check2_percent():
+    return round(random.uniform(0.73, 0.97), 2)
+
+# Function that checks if the digits are a hit or not
 def is_correct(digit1, digit2):
     diff = abs(digit1 - digit2)
-    return diff == 0 or diff == 1
+    if diff == 0 or diff == 1:
+        return True
+    else:
+        return False
 
+# Function that generates the first check number
+def check1_generator(hit_interval):
+    percent = generate_check1_percent()
+    mult = math.ceil(percent * hit_interval)
+    return mult
+
+# Function that generates the first check number
+def check2_generator(hit_interval):
+    percent = generate_check2_percent()
+    mult = math.ceil(percent * hit_interval)
+    return mult
+
+def array_append(no_double, digit1, digit2):
+    if(is_correct(digit1, digit2) == True):
+        no_double.append(True)
+    else:
+        no_double.append(False)
+    if len(no_double) > 2:
+        no_double.pop(0)
+
+#-------------[Declaring Variables]-------------#
 # Create a window
 win = visual.Window([800,600], monitor="testMonitor", units="deg")
 
@@ -22,37 +54,84 @@ text = visual.TextStim(win)
 
 # setting the beginning variables
 count = 1
+max_count = 900
+
+hit_interval = 30
+count_per_interval = 1
+
 num_hits = 0
 max_hits = 2
 
-# Main experiment loop
-while True:
+no_double = []
+
+# Generate the first and second check numbers
+check1 = check1_generator(hit_interval)
+check2 = check2_generator(hit_interval)
+
+#-------------[Main Experiment Loop]-------------#
+while True:    
     # making sure that the program stops after 900 nums
-    if count > 900:
+    if count > max_count:
         break
-
-    # Generate random double digit
-    digit = generate_double_digit()
-
-    # Generate another random double digit
-    next_digit = generate_double_digit()
     
-    # counting the amount of hits
-    if is_correct(digit, next_digit):
+    # Generate random double digits
+    digit = generate_digit()
+    next_digit = generate_digit()
+    
+    # Counting the amount of hits   
+    if (is_correct(digit, next_digit) == True):
         num_hits = num_hits + 1
     
-    # if the number of hits are larger than 2 then make a new number
-    # that does not generate a hit
-    if num_hits > max_hits:
-        while(is_correct(digit, next_digit)):
-            digit = generate_double_digit()
-            next_digit = generate_double_digit()
-
-    # restarting the hit count after 30 numbers
-    if count % 30 == 0:
+    # First check to see if we have hit at least once
+    if(num_hits < 1):
+        if(count_per_interval == check1):
+            while(is_correct(digit, next_digit) == False):
+                digit = generate_digit()
+                next_digit = generate_digit()
+            if(is_correct(digit, next_digit) == True):
+                num_hits = num_hits + 1        
+    
+    # Second check to see if we have hit twice
+    if(num_hits < 2):
+        if(count_per_interval == check2):
+            while(is_correct(digit, next_digit) == False):
+                digit = generate_digit()
+                next_digit = generate_digit()
+            if(is_correct(digit, next_digit) == True):
+                num_hits = num_hits + 1
+    
+    
+    array_append(no_double, digit, next_digit)
+    # Check for doubles
+    if(no_double[0] == True and no_double[1] == True):
+        num_hits -= 1
+        no_double[1] = False
+        while(is_correct(digit, next_digit) == True):
+            digit = generate_digit()
+            next_digit = generate_digit()
+    
+    # Check to make sure we don't go over 2 hits
+    if (num_hits > max_hits):
+        num_hits -= 1
+        while(is_correct(digit, next_digit) == True):
+            digit = generate_digit()
+            next_digit = generate_digit()
+    
+    # Resetting hit count after 30 numbers
+    if count % hit_interval == 0:
         num_hits = 0
     
+    # Resetting check numbers
+    if count_per_interval == hit_interval:
+        check1 = check1_generator(hit_interval)
+        check2 = check2_generator(hit_interval)
 
+    # Resetting interval count
+    if count_per_interval > hit_interval:
+        count_per_interval = 1               
+    
+    #-------------[Display + Input Section]-------------#
+    
     # putting digit and next_digit together
     new_digit = str(digit) + str(next_digit)
     text.text = str(new_digit)
@@ -84,4 +163,5 @@ while True:
         win.flip()
         core.wait(1)
     
-        count = count + 1
+        count += 1  
+        count_per_interval += 1
